c5bf4002d18a595dfeaa4f20c2cec58177ceaa2f
AzmiAli96 <azmialid@gmail.com>
2023-03-30 10:04:54 +0700

    30/03/23

---
+++ src/Azmi_300323/Aritmatika.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Azmi_300323;
+
+/**
+ *
+ * @author nitro
+ */
+public class Aritmatika {
+    public static int tambah (int a, int b){
+        return a+b;
+    }
+    
+    public static boolean ckganjil(int a){
+        return (a%2 != 0);
+    }
+    public void ganjil(int n){
+        int i =0;
+        int j =1;
+        while (i<=n){
+            if (j%2 != 0);
+            System.out.println(j+", ");
+            i++;
+        }
+        j++;
+    }
+    System.out.println();
+}
---
+++ src/Azmi_300323/AritmatikaExample.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Azmi_300323;
+
+/**
+ *
+ * @author nitro
+ */
+public class AritmatikaExample {
+        public static void main (String []args){
+            Aritmatika ar = new Aritmatika;
+            int a = 1;
+            int b = 2;
+        }
+        System.out.println(tambah(a,b));
+        System.out.println(ckganjil(a));
+    
+}
